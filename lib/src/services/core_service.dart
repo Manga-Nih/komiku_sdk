2eebe0555ae94c5a666b818ce6454d51cd8ddc70
zalviandyr <zukronalviandy@gmail.com>
2023-01-19 10:10:26 +0700

    feat: remove meta

--- lib/src/services/core_service.dart
+++ lib/src/services/core_service.dart
@@ -1,9 +1,7 @@
-import 'package:meta/meta.dart';
 import 'package:universal_html/controller.dart';
 import 'package:universal_html/html.dart';
 
 abstract class CoreService {
-  @protected
   Future<Document?> getBodyFromUrl(String url) async {
     final WindowController _controller = WindowController();
     await _controller.openHttp(uri: Uri.parse(url));
